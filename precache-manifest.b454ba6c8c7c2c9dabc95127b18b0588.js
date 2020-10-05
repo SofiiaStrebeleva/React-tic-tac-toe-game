d650963cd73092bef1f66e859e037af5e51af40e
sofiiastrebeleva <sofiia.strebeleva@gmail.com>
2020-10-05 11:57:52 +0300

    Updates

---
+++ precache-manifest.b454ba6c8c7c2c9dabc95127b18b0588.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a404c083818a4a4daf4489674849efd",
+    "url": "/React-tic-tac-toe-game/index.html"
+  },
+  {
+    "revision": "8cf41922f132afa31d49",
+    "url": "/React-tic-tac-toe-game/static/css/main.157abfbc.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/React-tic-tac-toe-game/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-tic-tac-toe-game/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cf41922f132afa31d49",
+    "url": "/React-tic-tac-toe-game/static/js/main.aee0d02c.chunk.js"
+  },
+  {
+    "revision": "db0cb825fd15fad4f560",
+    "url": "/React-tic-tac-toe-game/static/js/runtime-main.8a5d52fa.js"
+  }
+]);
